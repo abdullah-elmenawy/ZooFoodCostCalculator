1d78ac0e87a26ac4e3e1701af9cc77c0dfab68eb
Abdullah El-Menawy <a.elmenawy@outlook.com>
2025-04-22 17:13:45 +0200

    Fixing return type issue

--- ZooFoodCostCalculator.Console/ZooApp.cs
+++ ZooFoodCostCalculator.Console/ZooApp.cs
@@ -7,13 +7,13 @@ namespace ZooFoodCostCalculator.Console
         public async Task RunAsync()
         {
             System.Console.WriteLine("Welcome to Zoo Food Cost Calculator");
-            var totalCost = await zooApplicationService.CalculateFoodCost();
+            var result = await zooApplicationService.CalculateFoodCost();
 
             System.Console.WriteLine();
             System.Console.ForegroundColor = ConsoleColor.DarkYellow;
-         
-            System.Console.WriteLine("Total food cost for the Zoo is : {0}$", totalCost);
-            
+
+            System.Console.WriteLine("Total food cost for the Zoo is : {0}$", result.Value);
+
             System.Console.WriteLine();
             System.Console.ResetColor();
 
